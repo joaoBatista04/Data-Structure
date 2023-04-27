f4c70c00cf167e385c22e510a3aa5220d50900a2
Joao Batista <jpcbatista.2004@gmail.com>
2023-04-27 08:23:49 -0300

    ⛓️⛓️ Creating a new data structure: doubly linked list

---
+++ List/list.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
+#include "list.h"
+
+struct List{
+    int size;
+    Node *head;
+    Node *last;
+    Node *previous;
+};
+
+/**
+ * @brief This function creates a doubly linked list and allocates memory to it
+ * 
+ * @return List* 
+ */
+List* list_construct(){
+    List *fw = malloc(sizeof(List));
+
+    fw->size = 0;
+    fw->head = NULL;
+    fw->last = NULL;
+    fw->previous = NULL;
+
+    return fw;
+}
+
+/**
+ * @brief This function returns the doubly linked list size
+ * 
+ * @param l 
+ * @return int 
+ */
+int list_size(List* l){
+    return l->size;
+}
+
+/**
+ * @brief This function adds an item to the beginning of the list
+ * 
+ * @param l 
+ * @param val 
+ */
+void list_push_front(List* l, data_type val){
+    Node *n = node_construct(val, l->head);
+    
+    l->head = n;
+    l->size++;
+
+    if(l->size == 1){
+        l->last = l->head;
+    }
+}
---
+++ List/list.h
@@ -0,0 +1,31 @@
+#ifndef List_H_
+#define List_H_
+#include "node.h"
+
+typedef struct List List;
+
+//This function creates a linked list and allocates memory to it
+List* list_construct();
+//This function returns the linked list size
+int list_size(List* l);
+//This function adds an item to the beginning of the list
+void list_push_front(List* l, data_type val);
+//This function prints the linked list values on the screen. This function needs a function pointer that tells how to print the data_type
+void list_print(List* l, void (*print_fn)(data_type));
+//This function returns the i-th element of the linked list
+data_type list_get(List* l, int i);
+//This function removes the first element from the linked list
+data_type list_pop_front(List* l);
+//This function returns a linked list containing the reverse of another
+List* list_reverse(List* l);
+//This function returns the same reversed linked list, without creating a new one
+List* list_reverse_inplace(List* l);
+//This function frees the memory allocated to the list and its nodes
+void list_destroy(List *l);
+//This function concatenates two linked list, adding the second one values at the beginning of the first one
+void list_cat(List *list, List *listTwo);
+//This function adds an item to the end of the list
+void list_push_back(List *l, data_type value);
+
+
+#endif
