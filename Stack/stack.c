48a7c69eab8e584ef810680cb8471fa8615b563b
Joao Batista <jpcbatista.2004@gmail.com>
2023-05-16 11:23:52 -0300

    📚 Updating comments about the code

--- Stack/stack.c
+++ Stack/stack.c
@@ -8,7 +8,11 @@ typedef struct Stack{
     int size;
 } Stack;
 
-// cria uma stack
+/**
+ * @brief This function builds a Stack type and allocates memory for it
+ * 
+ * @return Stack* 
+ */
 Stack *stack_construct(){
     Stack *stack = (Stack *)malloc(sizeof(Stack));
 
@@ -18,13 +22,23 @@ Stack *stack_construct(){
     return stack;
 }
 
-// insere um item na stack
+/**
+ * @brief This function adds a new value to the top of the Stack
+ * 
+ * @param stack 
+ * @param val 
+ */
 void stack_push(Stack *s, data_type val){
     forward_list_push_front(s->forwardList, val);
     s->size++;
 }
 
-// remove o ultimo item inserido e o retorna
+/**
+ * @brief This function removes the value at the top of the Stack
+ * 
+ * @param stack
+ * @return data_type 
+ */
 data_type stack_pop(Stack *s){
     data_type val;
     val = forward_list_pop_front(s->forwardList);
@@ -33,7 +47,12 @@ data_type stack_pop(Stack *s){
     return val;
 }
 
-// retorna 1 se a stack está vazia e 0 caso contrário
+/**
+ * @brief This function returns 1 if the Stack is empty and 0 if the Stack contains at least one value
+ * 
+ * @param stack 
+ * @return int 
+ */
 int stack_empty(Stack *s){
     if(!s->size){
         return 1;
@@ -44,7 +63,11 @@ int stack_empty(Stack *s){
     }
 }
 
-// libera o espaço alocado para a stack
+/**
+ * @brief This function frees the memory allocated to a Stack
+ * 
+ * @param stack
+ */
 void stack_destroy(Stack *s){
     forward_list_destroy(s->forwardList);
 
--- Stack/stack.h
+++ Stack/stack.h
@@ -5,10 +5,42 @@
 typedef int data_type;
 typedef struct Stack Stack;
 
+/**
+ * @brief This function builds a Stack type and allocates memory for it
+ * 
+ * @return Stack* 
+ */
 Stack *stack_construct();
-void stack_push(Stack *s, data_type val);
-data_type stack_pop(Stack *s);
-int stack_empty(Stack *s);
-void stack_destroy(Stack *s);
+
+/**
+ * @brief This function adds a new value to the top of the Stack
+ * 
+ * @param stack 
+ * @param val 
+ */
+void stack_push(Stack *stack, data_type val);
+
+/**
+ * @brief This function removes the value at the top of the Stack
+ * 
+ * @param stack
+ * @return data_type 
+ */
+data_type stack_pop(Stack *stack);
+
+/**
+ * @brief This function returns 1 if the Stack is empty and 0 if the Stack contains at least one value
+ * 
+ * @param stack 
+ * @return int 
+ */
+int stack_empty(Stack *stack);
+
+/**
+ * @brief This function frees the memory allocated to a Stack
+ * 
+ * @param stack
+ */
+void stack_destroy(Stack *stack);
 
 #endif
